27b9b4cd726effe29353c4672e68b1a37b9b6f20
Bhandari Sambhav <sambhavbhandari@Bhandaris-MacBook-Air.local>
2023-07-19 06:41:16 +0800

    initial page

---
+++ Hello.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AMZNSLAPPS",
+    page_icon="👋",
+)
+
+st.header("AMZN SLAPPS! 👋")
+st.subheader("or AMaZoN StreamLit APPlicationS")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    This website hosts publicly accessible functions meant for
+    automating mundane tasks.
+    **👈 Select an option from the sidebar** to see some examples
+    of what to use it for!
+    ### Want to learn more?
+    - 🔜 Loom video tutorials coming soon!
+"""
+)
